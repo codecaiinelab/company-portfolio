33ec3f482fab01dda354a2d60f712c79bba13b2e
codecaiine <akabrouyannickn@gmail.com>
2022-08-30 14:24:05 +0000

    Import and use useefect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react';
 
-import { Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
+  const handleClose =()=> setNav(!nav)
+
   return (
        <div className='w-screen h-[70px] z-10 bg-zinc-200 fixed'>
           <div className='px-2 flex justify-between items-center w-full h-full'>
@@ -21,12 +26,12 @@ const Navbar = () => {
               <button className='px-8 py-3'>Quotation</button>
             </div>
             <div className='md:hidden mr-4'>
-                <Bars3Icon className='w-5' />
+                {!nav ? <Bars3Icon className='w-5' />: <XMarkIcon className='w-5' />}
             </div>
          </div>
 
 
-         <ul className='absolute bg-zinc-200 w-full px-8'>
+         <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
           <li className='border-b-2 border-zinc-300 w-full'>Home</li>
           <li className='border-b-2 border-zinc-300 w-full'>About</li>
           <li className='border-b-2 border-zinc-300 w-full'>Services</li>
