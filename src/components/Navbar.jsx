9ec8f1bfe25eb1841c693fe0abdb046112294e51
codecaiine <akabrouyannickn@gmail.com>
2022-08-30 14:18:00 +0000

    Finalize navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React from 'react'
 
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Bars3Icon } from '@heroicons/react/24/solid'
+
 
 const Navbar = () => {
   return (
-    <div className='w-screen h-[70px] z-10 bg-zinc-200 fixed'>
-        <div className='px-2 flex justify-between items-center w-full h-full'>
-            <div className='flex items-center'>
+       <div className='w-screen h-[70px] z-10 bg-zinc-200 fixed'>
+          <div className='px-2 flex justify-between items-center w-full h-full'>
+             <div className='flex items-center'>
                 <h1 className='font-bold text-[20px]'>Del Consult</h1>
                 <ul className='hidden md:flex'>
-                     <li>Home</li>
-                     <li>About</li>
-                     <li>Services</li>
-                     <li>Projects</li>
-                     <li>Contact</li>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
                 </ul>
+             </div>
+            <div className='hidden md:flex pr-4'>
+              <button className='px-8 py-3'>Quotation</button>
             </div>
-        </div>
-    </div>
+            <div className='md:hidden mr-4'>
+                <Bars3Icon className='w-5' />
+            </div>
+         </div>
+
+
+         <ul className='absolute bg-zinc-200 w-full px-8'>
+          <li className='border-b-2 border-zinc-300 w-full'>Home</li>
+          <li className='border-b-2 border-zinc-300 w-full'>About</li>
+          <li className='border-b-2 border-zinc-300 w-full'>Services</li>
+          <li className='border-b-2 border-zinc-300 w-full'>Projects</li>
+          <li className='border-b-2 border-zinc-300 w-full'>Contact</li>
+
+          <div className='flex flex-col my-4'>
+              <button className='px-8 py-3'>Quotation</button>
+          </div>
+        </ul>
+      </div>
   )
 }
 
