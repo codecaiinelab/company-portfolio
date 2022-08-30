0519308621790d2e6f5d26d475fc115121de7438
codecaiine <akabrouyannickn@gmail.com>
2022-08-30 14:27:36 +0000

    Finalize our mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
             <div className='hidden md:flex pr-4'>
               <button className='px-8 py-3'>Quotation</button>
             </div>
-            <div className='md:hidden mr-4'>
+            <div className='md:hidden mr-4' onClick={handleClick}>
                 {!nav ? <Bars3Icon className='w-5' />: <XMarkIcon className='w-5' />}
             </div>
          </div>
