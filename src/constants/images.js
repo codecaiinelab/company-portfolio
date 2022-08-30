243d7b926fa8aea232c0d098f3283ea15495d800
codecaiine <akabrouyannickn@gmail.com>
2022-08-30 12:22:36 +0000

    Ewport images

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import building from '../assets/building.jpg';
+import construction from '../assets/construction.jpg';
+import engineer from '../assets/engineer.jpg';
+import engineer_material from '../assets/engineer-material.png';
+
+export default {
+    building,
+    construction,
+    engineer,
+    engineer_material,
+};
