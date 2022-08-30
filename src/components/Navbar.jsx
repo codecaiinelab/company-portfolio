8b61a27ac7c384247a1501fc24f44cb24822b826
codecaiine <akabrouyannickn@gmail.com>
2022-08-30 13:40:55 +0000

    Add list options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,15 @@ const Navbar = () => {
         <div className='px-2 flex justify-between items-center w-full h-full'>
             <div className='flex items-center'>
                 <h1 className='font-bold text-[20px]'>Del Consult</h1>
+                <ul className='hidden md:flex'>
+                     <li>Home</li>
+                     <li>About</li>
+                     <li>Services</li>
+                     <li>Projects</li>
+                     <li>Contact</li>
+                </ul>
             </div>
         </div>
-
-        <MenuIcon className='w-5'/>
     </div>
   )
 }
