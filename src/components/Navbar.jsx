79d0836c3b219011706c6a7a4665be368aaad8a5
codecaiine <akabrouyannickn@gmail.com>
2022-08-30 13:30:50 +0000

    Add navigation and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-screen h-[70px] z-10 bg-zinc-200 fixed'>
+        <div className='px-2 flex justify-between items-center w-full h-full'>
+            <div className='flex items-center'>
+                <h1 className='font-bold text-[20px]'>Del Consult</h1>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
